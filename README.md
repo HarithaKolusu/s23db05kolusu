28ba8904339d504efc495314cd1f0fdd5ae151e8
s564205 <s564205@nwmissouri.edu>
2023-10-30 23:58:12 -0500

    Added Orders

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ Take a momemt to  take care of yourself
 
 [link to render server](https://f23wb05kolusu.onrender.com)
 
-class Orders{ String name; integer id; integer price; public(String name, integer id, integer price) { this.name=name; this.id=id; this.price= price;} }
+class Orders{String name; integer id; integer price; public(String name, integer id, integer price) { this.name=name; this.id=id; this.price= price;}}
--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var OrdersRouter = require('./routes/Orders');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Orders', OrdersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
