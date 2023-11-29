7ca2017f62d6229aa9ba7bcc08557ebfff0bd331
s564205 <s564205@nwmissouri.edu>
2023-11-28 23:18:28 -0600

    updated Orders

--- routes/Orders.js
+++ routes/Orders.js
@@ -10,14 +10,14 @@ const secured = (req, res, next) => {
 }
 
 /* GET home page. */
-router.get('/', Orders_controller.Orders_view_all_Page );
+router.get('/', secured,Orders_controller.Orders_view_all_Page );
 /* GET detail Orders page */
-router.get('/detail', Orders_controller.Orders_view_one_Page);
+router.get('/detail', secured, Orders_controller.Orders_view_one_Page);
 /* GET create Orders page */
-router.get('/create', Orders_controller.Orders_create_Page);
+router.get('/create', secured, Orders_controller.Orders_create_Page);
 /* GET create update page */
-router.get('/update', Orders_controller.Orders_update_Page);
+router.get('/update', secured,Orders_controller.Orders_update_Page);
 /* GET delete costume page */
-router.get('/delete', Orders_controller.Orders_delete_Page);
+router.get('/delete',secured, Orders_controller.Orders_delete_Page);
 
 module.exports = router;
